9a8c4915e1283adf9f208fbe4320b217893f83b9
tpphu <vietean@gmail.com>
2015-10-09 02:09:29 +0700

    Demo how java script load and execute

--- javascript-on-browser/js3/example1.js
+++ javascript-on-browser/js3/example1.js
@@ -1,6 +1,9 @@
 console.log('From Example 1');
 
+var start = new Date();
+var diff  = 0;
 for(var i = 0; i < 10000; i++)
 {
-	console.log('From Example 1: ', i);
+	diff = new Date() - start;
+	console.log('From Example 1: ', i, ' | diff: ', diff);
 }
