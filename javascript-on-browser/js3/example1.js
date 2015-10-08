090d721c55836e3c040b62b5bcc65130102763c4
tpphu <vietean@gmail.com>
2015-10-09 02:11:54 +0700

    Demo how java script load and execute

--- javascript-on-browser/js3/example1.js
+++ javascript-on-browser/js3/example1.js
@@ -1,5 +1,13 @@
 console.log('From Example 1');
 
+var beginCallback = new Date();
+setTimeout(function(){
+	console.log('From Example 1 | Callback: \
+		Muon dc goi vao 250ms sau do | nhung thoi thuc te la:',
+		new Date() - beginCallback
+	);
+}, 250);
+
 var start = new Date();
 var diff  = 0;
 for(var i = 0; i < 10000; i++)
